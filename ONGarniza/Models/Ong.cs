df6a32f115cfb73cd86c743d2cf59a2d90d31fc1
Gabriel Oliveira <gabrieloliveira@frwk.com.br>
2021-11-04 11:40:27 -0300

    Corrigindo propriedades ongs

--- ONGarniza/Models/Ong.cs
+++ ONGarniza/Models/Ong.cs
@@ -8,11 +8,11 @@ namespace ONGarniza.Models
     public class Ong
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Name { get; set; }
         public string Cnpj { get; set; }
         public string Email { get; set; }
-        public string Telefone { get; set; }
-        public string Endereco  { get; set; }
-        public string Senha { get; set; }
+        public string Phone { get; set; }
+        public string Address  { get; set; }
+        public string Password { get; set; }
     }
 }
